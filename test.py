74946bd9e0fb65f571b392ba8558f092d61deeda
Sahe20 <jackitehm@gmail.com>
2024-04-08 16:08:03 +0300

    Reference count in python

--- test.py
+++ test.py
@@ -1,14 +1,19 @@
+import sys
 #print ("Hello, World!")
-# mutable and immutable object ex:
+#mutable and immutable object ex:
 
-my_list = [1,2,3]
-print (id(my_list))  
+# my_list = [1,2,3]
+# print (id(my_list))  
 
-my_list.append(4)
-print(id(my_list)) # mutable obj
+# my_list.append(4)
+# print(id(my_list)) # mutable obj
 
-my_tuple = (1,2,3)
-print(id(my_tuple)) 
+# my_tuple = (1,2,3)
+# print(id(my_tuple)) 
 
-my_tuple += (4,)
-print(id(my_tuple)) #immutable obj
+# my_tuple += (4,)
+# print(id(my_tuple)) #immutable obj
+
+#reference count ex:
+a=300 
+print(sys.getrefcount(a))
