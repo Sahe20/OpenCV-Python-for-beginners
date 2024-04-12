2d544da790d9cf486d6aedf2bf39c558a9f2eddc
Sahe20 <jackitehm@gmail.com>
2024-04-12 16:55:11 +0300

    Intro to conditionals; if, elif, and else

--- test.py
+++ test.py
@@ -94,5 +94,21 @@ import math
 # x = help(False)
 # print(x)
 
-x = help()
-print(x)
+# x = help()
+# print(x)
+
+                        # Conditionals
+
+grade = 98
+if grade >= 95:
+    print('A+')
+if grade >= 90:
+    print('A')
+elif grade >= 80:
+    print('B')
+elif grade >= 70:
+    print('C')
+elif grade >= 60:
+    print('D')
+else:
+    print('F') 
