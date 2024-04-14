ec93b4a43f86c3e68831fbb2a7ce3d38c7bb098b
Sahe20 <jackitehm@gmail.com>
2024-04-14 12:04:24 +0300

    Comprehension creating upper case letter

--- test.py
+++ test.py
@@ -316,3 +316,6 @@ numbers = [1, 2, 3, 4, 5, 6]
 even_numbers = [x for x in numbers if x % 2 == 0]
 print(even_numbers)
 
+text = 'Hello Guys'
+uppercase_text = [letter.upper() for letter in text]
+print(uppercase_text)
