d42962031494dff2b3d4ff2e20ec2a64f61f558b
Sahe20 <jackitehm@gmail.com>
2024-04-14 12:03:31 +0300

    Comprehension finding even numbers

--- test.py
+++ test.py
@@ -311,3 +311,8 @@ import math
 numbers = [1, 2, 3, 4, 5]
 squares = [x**2 for x in numbers]
 print(squares)
+
+numbers = [1, 2, 3, 4, 5, 6]
+even_numbers = [x for x in numbers if x % 2 == 0]
+print(even_numbers)
+
